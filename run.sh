c1a3e2adf3709d568e140da9cd3de5d894451849
ifapmzadu6 <ifapmzadu6@gmail.com>
2016-03-24 20:41:47 +0900

    remove comment

--- run.sh
+++ run.sh
@@ -6,7 +6,6 @@ mkdir output
 
 for path in $(ls -l | awk '$1 ~ /d/ {print $9 }')
 do
-#    for file in $(find ${DIR}/${path} -name "_48dp")
     cp ${DIR}/${path}/drawable-xxxhdpi/*_black_48dp.png ./output
 done
 
